b585638e8d330a43f3a5836ad3cca68f8c4f3261
Julian Arz <arzjulian@googlemail.com>
2018-12-04 00:05:07 +0100

    increased version to 0.0.5

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -5,7 +5,7 @@ import os
 
 from flask import Flask, render_template
 
-version_info = (0, 0, 4)
+version_info = (0, 0, 5)
 __version__ = '.'.join(map(str, version_info))
 
 def create_app(test_config=None):
