3b65d1936748a10f9e1091f24a22752175d3f16c
Julian Arz <arzjulian@googlemail.com>
2018-12-30 14:05:57 +0100

    web-client: setting linter line-length to 100

--- web-client/.eslintrc.js
+++ web-client/.eslintrc.js
@@ -7,9 +7,16 @@ module.exports = {
     rules: {
         //"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-console": "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "max-len": [
+            1,
+            {
+                code: 100,
+                tabWidth: 2
+            }
+        ]
     },
     parserOptions: {
         parser: "babel-eslint"
-    },
+    }
 };
