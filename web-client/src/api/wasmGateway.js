362678f325abc9a9b74947e883494350a8309a89
Julian Arz <j.arz@web.de>
2020-07-31 21:09:01 +0200

    web-client: put low-level wasm code into a module

---
+++ web-client/src/api/wasmGateway.js
@@ -0,0 +1,121 @@
+export default class WasmGateway {
+    constructor() {
+        this.libexhsearch = null;
+        this.isLoading = false;
+        this.publicPath = process.env.BASE_URL;
+    }
+
+    loadLibexhsearch(callback) {
+        if (!this.libexhsearch && !this.isLoading) {
+            this.isLoading = true;
+            this._loadRuntime()
+                .then(loadWasm => {
+                    loadWasm().then(libexhsearch => {
+                        this.libexhsearch = libexhsearch;
+                        this.isLoading = false;
+                        callback();
+                    });
+                })
+                .catch(reason => {
+                    console.error("failed to load runtime: " + reason);
+                    this.isLoading = false;
+                })
+                .then(() => {
+                    this.isLoading = false;
+                });
+        }
+    }
+
+    computeActions(game, playerId) {
+        let computedActions = null;
+        let player = game.getPlayer(playerId);
+        let objectiveId = -1;
+        let vectorNodes = new this.libexhsearch.vectorOfNode();
+        let mazeCardList = game.mazeCardsAsList().concat([game.leftoverMazeCard]);
+        for (let mazeCard of mazeCardList) {
+            vectorNodes.push_back(this._createNode(mazeCard));
+            if (mazeCard.hasObject) {
+                objectiveId = mazeCard.id;
+            }
+        }
+
+        let mazeGraph = new this.libexhsearch.MazeGraph(vectorNodes);
+        let border = game.n - 1;
+        for (var position = 1; position < border; position += 2) {
+            mazeGraph.addShiftLocation(this._loc(0, position));
+            mazeGraph.addShiftLocation(this._loc(position, 0));
+            mazeGraph.addShiftLocation(this._loc(position, border));
+            mazeGraph.addShiftLocation(this._loc(border, position));
+        }
+
+        let previousShiftLocation = this._loc(-1, -1);
+        if (game.disabledShiftLocation) {
+            previousShiftLocation = game.getOppositeLocation(game.disabledShiftLocation);
+        }
+
+        let action = this.libexhsearch.findBestAction(
+            mazeGraph,
+            player.mazeCard.location,
+            objectiveId,
+            previousShiftLocation
+        );
+
+        computedActions = {
+            shiftAction: {
+                location: this._loc(action.shift.location.row, action.shift.location.column),
+                leftoverRotation: action.shift.rotation
+            },
+            moveLocation: this._loc(action.move_location.row, action.move_location.column)
+        };
+
+        mazeGraph.delete();
+        for (let i = 0; i < vectorNodes.size(); ++i) {
+            vectorNodes.get(i).delete();
+        }
+        vectorNodes.delete();
+
+        return computedActions;
+    }
+
+    _createNode(mazeCard) {
+        let id = mazeCard.id;
+        let outPathBitmask = 0;
+        if (mazeCard.hasNorthOutPath()) {
+            outPathBitmask += 1;
+        }
+        if (mazeCard.hasEastOutPath()) {
+            outPathBitmask += 2;
+        }
+        if (mazeCard.hasSouthOutPath()) {
+            outPathBitmask += 4;
+        }
+        if (mazeCard.hasWestOutPath()) {
+            outPathBitmask += 8;
+        }
+        let rotation = mazeCard.rotation;
+        return new this.libexhsearch.Node(id, outPathBitmask, rotation);
+    }
+
+    _loc(row, column) {
+        return { row: row, column: column };
+    }
+
+    _loadRuntime() {
+        return new Promise((resolve, reject) => {
+            const url =
+                location.protocol + "//" + location.host + this.publicPath + "wasm/libexhsearch.js";
+            const script = document.createElement("script");
+            script.type = "text/javascript";
+            script.async = true;
+            script.addEventListener("load", () => {
+                resolve(window["libexhsearch"]);
+            });
+            script.addEventListener("error", () => {
+                reject(new Error(`Error loading ${url}`));
+            });
+            script.src = url;
+            document.body.appendChild(script);
+            this.scriptElement = script;
+        });
+    }
+}
--- web-client/src/components/WasmPlayer.vue
+++ web-client/src/components/WasmPlayer.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script>
-import { Game, loc, SHIFT_ACTION, MOVE_ACTION, NO_ACTION } from "@/model/game.js";
+import { Game, SHIFT_ACTION, MOVE_ACTION, NO_ACTION } from "@/model/game.js";
+import WasmGateway from "@/api/wasmGateway.js";
 
 export default {
     name: "wasm-player",
@@ -19,32 +20,39 @@ export default {
     },
     data() {
         return {
-            libexhsearch: null,
-            isLoading: false,
             computedAction: null,
-            publicPath: process.env.BASE_URL
+            wasmGateway: new WasmGateway()
         };
     },
     watch: {
         playerType: function(newType, oldType) {
             if (oldType !== "wasm" && newType === "wasm") {
-                if (!this.libexhsearch) {
-                    this.loadLibexhsearch(() => {
+                if (!this.wasmGateway.libexhsearch) {
+                    this.wasmGateway.loadLibexhsearch(() => {
                         if (this.playerTurnAction === SHIFT_ACTION) {
-                            this.computeActions();
+                            this.computedAction = this.wasmGateway.computeActions(
+                                this.game,
+                                this.playerId
+                            );
+                            this.$emit("perform-shift", this.computedAction.shiftAction);
                         }
                     });
                 } else {
                     if (this.playerTurnAction === SHIFT_ACTION) {
-                        this.computeActions();
+                        this.computedAction = this.wasmGateway.computeActions(
+                            this.game,
+                            this.playerId
+                        );
+                        this.$emit("perform-shift", this.computedAction.shiftAction);
                     }
                 }
             }
         },
         playerTurnAction: function(newAction, oldAction) {
-            if (this.libexhsearch) {
+            if (this.wasmGateway.libexhsearch) {
                 if (oldAction !== SHIFT_ACTION && newAction === SHIFT_ACTION) {
-                    this.computeActions();
+                    this.computedAction = this.wasmGateway.computeActions(this.game, this.playerId);
+                    this.$emit("perform-shift", this.computedAction.shiftAction);
                 } else if (
                     oldAction !== MOVE_ACTION &&
                     newAction === MOVE_ACTION &&
@@ -77,123 +85,12 @@ export default {
         }
     },
     methods: {
-        computeActions: function() {
-            this.computedAction = null;
-            let player = this.game.getPlayer(this.playerId);
-            let objectiveId = -1;
-            let vectorNodes = new this.libexhsearch.vectorOfNode();
-            let mazeCardList = this.game.mazeCardsAsList().concat([this.game.leftoverMazeCard]);
-            for (let mazeCard of mazeCardList) {
-                vectorNodes.push_back(this.createNode(mazeCard));
-                if (mazeCard.hasObject) {
-                    objectiveId = mazeCard.id;
-                }
-            }
-
-            let mazeGraph = new this.libexhsearch.MazeGraph(vectorNodes);
-            let border = this.game.n - 1;
-            for (var position = 1; position < border; position += 2) {
-                mazeGraph.addShiftLocation(loc(0, position));
-                mazeGraph.addShiftLocation(loc(position, 0));
-                mazeGraph.addShiftLocation(loc(position, border));
-                mazeGraph.addShiftLocation(loc(border, position));
-            }
-
-            let previousShiftLocation = loc(-1, -1);
-            if (this.game.disabledShiftLocation) {
-                previousShiftLocation = this.game.getOppositeLocation(
-                    this.game.disabledShiftLocation
-                );
-            }
-
-            let action = this.libexhsearch.findBestAction(
-                mazeGraph,
-                player.mazeCard.location,
-                objectiveId,
-                previousShiftLocation
-            );
-
-            this.computedAction = {
-                shiftAction: {
-                    location: loc(action.shift.location.row, action.shift.location.column),
-                    leftoverRotation: action.shift.rotation
-                },
-                moveLocation: loc(action.move_location.row, action.move_location.column)
-            };
-
-            mazeGraph.delete();
-            for (let i = 0; i < vectorNodes.size(); ++i) {
-                vectorNodes.get(i).delete();
-            }
-            vectorNodes.delete();
-
-            this.$emit("perform-shift", this.computedAction.shiftAction);
-        },
-        createNode: function(mazeCard) {
-            let id = mazeCard.id;
-            let outPathBitmask = 0;
-            if (mazeCard.hasNorthOutPath()) {
-                outPathBitmask += 1;
-            }
-            if (mazeCard.hasEastOutPath()) {
-                outPathBitmask += 2;
-            }
-            if (mazeCard.hasSouthOutPath()) {
-                outPathBitmask += 4;
-            }
-            if (mazeCard.hasWestOutPath()) {
-                outPathBitmask += 8;
-            }
-            let rotation = mazeCard.rotation;
-            return new this.libexhsearch.Node(id, outPathBitmask, rotation);
-        },
         player: function() {
             return (
                 this.game.nextAction.playerId === this.playerId &&
                 this.game.nextAction.action === SHIFT_ACTION &&
                 !this.game.getPlayer(this.userPlayerId).isComputer
             );
-        },
-        loadLibexhsearch: function(callback) {
-            if (!this.isLoading) {
-                this.isLoading = true;
-                this.loadRuntime()
-                    .then(loadWasm => {
-                        loadWasm().then(libexhsearch => {
-                            this.libexhsearch = libexhsearch;
-                            this.isLoading = false;
-                            callback();
-                        });
-                    })
-                    .catch(reason => {
-                        console.error("failed to load runtime: " + reason);
-                        this.isLoading = false;
-                    })
-                    .then(() => {
-                        this.isLoading = false;
-                    });
-            }
-        },
-        loadRuntime: function() {
-            return new Promise((resolve, reject) => {
-                const url =
-                    location.protocol +
-                    "//" +
-                    location.host +
-                    this.publicPath +
-                    "wasm/libexhsearch.js";
-                const script = document.createElement("script");
-                script.type = "text/javascript";
-                script.async = true;
-                script.addEventListener("load", () => {
-                    resolve(window["libexhsearch"]);
-                });
-                script.addEventListener("error", () => {
-                    reject(new Error(`Error loading ${url}`));
-                });
-                script.src = url;
-                document.body.appendChild(script);
-            });
         }
     }
 };
