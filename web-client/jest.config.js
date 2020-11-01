4fb738122a822363a2e1df65cffa3dfd721fc182
Julian Arz <j.arz@web.de>
2020-11-01 01:12:56 +0100

    gets jest tests to run after vue-cli upgrade

--- web-client/jest.config.js
+++ web-client/jest.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-    moduleFileExtensions: ["js", "jsx", "json", "vue"],
-    transform: {
-        "^.+\\.vue$": "<rootDir>/node_modules/vue-jest",
-        ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "<rootDir>/node_modules/jest-transform-stub",
-        "^.+\\.jsx?$": "<rootDir>/node_modules/babel-jest"
-    },
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1"
-    },
-    snapshotSerializers: ["jest-serializer-vue"],
+    preset: '@vue/cli-plugin-unit-jest',
     testMatch: ["**/tests/unit/**/*Test.(js|jsx|ts|tsx)"],
-    testURL: "http://localhost/",
-    collectCoverage: false,
-    collectCoverageFrom: ["**/src/**.{js,vue}", "!**/node_modules/**"]
 };
