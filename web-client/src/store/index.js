8374f436cdbffe66452d6913a5c65dd2e9a0e692
Julian Arz <j.arz@web.de>
2021-10-03 00:14:54 +0200

    adds countdown to Vuex modules

--- web-client/src/store/index.js
+++ web-client/src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 import game from "./modules/game.js";
 import board from "./modules/board.js";
 import players from "./modules/players.js";
+import countdown from "./modules/countdown.js";
 
 Vue.use(Vuex);
 
@@ -11,7 +12,8 @@ export default new Vuex.Store({
     modules: {
         game: game,
         board: board,
-        players: players
+        players: players,
+        countdown: countdown
     },
     strict: process.env.NODE_ENV !== "production"
 });
---
+++ web-client/src/store/modules/countdown.js
@@ -0,0 +1,62 @@
+export const state = () => ({
+    timer: 0,
+    remainingSeconds: 0,
+    startSeconds: 30
+});
+
+const getters = {
+    isRunning: state => {
+        state.timer !== 0;
+    }
+};
+const actions = {
+    restartCountdown({ state, commit, dispatch }) {
+        dispatch("stopCountdown");
+        commit("resetRemainingSeconds");
+        const timer = setInterval(() => {
+            commit("countDown");
+            if (state.remainingSeconds <= 0) {
+                dispatch("stopCountdown");
+                commit("clearRemainingSeconds");
+            }
+        }, 1000);
+        commit("saveTimer", timer);
+    },
+    stopCountdown({ state, commit, getters }) {
+        if (getters.isRunning) {
+            clearInterval(state.timer);
+            commit("clearTimer");
+        }
+    },
+    setDuration({ commit }, seconds) {
+        commit("setDurationSeconds", seconds);
+    }
+};
+const mutations = {
+    setDurationSeconds(state, seconds) {
+        state.startSeconds = seconds;
+    },
+    saveTimer(state, timer) {
+        state.timer = timer;
+    },
+    clearTimer(state) {
+        state.timer = 0;
+    },
+    resetRemainingSeconds(state) {
+        state.remainingSeconds = state.startSeconds;
+    },
+    clearRemainingSeconds(state) {
+        state.remainingSeconds = 0;
+    },
+    countDown(state) {
+        state.remainingSeconds--;
+    }
+};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+};
