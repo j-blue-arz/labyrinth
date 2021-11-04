f75164881562d24a0999553209bac7787b00485c
Julian Arz <j.arz@web.de>
2021-11-04 22:15:42 +0100

    increases version to 0.3.1

--- backend/labyrinth/__init__.py
+++ backend/labyrinth/__init__.py
@@ -4,7 +4,7 @@ and defines a set of API methods to play the game """
 from collections import namedtuple
 
 Version = namedtuple("Version", ["milestone", "major", "minor"])
-version_info = Version(0, 3, 0)
+version_info = Version(0, 3, 1)
 __version__ = '.'.join(map(str, version_info))
 
 
